175b6ddd973895e9f63c5b3500aa75ede260b4a3
Winifreddo <73237857+Winifreddo@users.noreply.github.com>
2021-02-06 13:07:44 +0000

    Added Form

---
+++ src/Form.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Form.css";
+export default function Form() {
+
+
+return (
+  <div className="Form">
+        <form>
+          <div className="row">
+            <div className="col-8">
+<div className="search">
+              <input
+                type="search" 
+                placeholder="Enter a City 🔎"
+                autoFocus="on"
+                autoComplete="off"
+                className="form-control sm"
+              />
+            </div>
+            </div>
+            
+            <div className="col-3">
+              <input
+                type="submit" 
+                value="Search"
+                className="btn btn-dark w-100"
+             />
+              </div>
+            
+            </div>
+        </form>
+      </div>
+        )
+}
